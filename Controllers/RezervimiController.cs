869fddf8a72121cd4ff6558803e2976e1be72184
Blerina Maxharraj <bm45555@ubt-uni.net>
2021-01-18 17:59:45 +0100

    CRUD-REZ

--- Controllers/RezervimiController.cs
+++ Controllers/RezervimiController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Libraryms.Data;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Libraryms.Models;
+using Libraryms.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Libraryms.Controllers
 {
@@ -27,14 +29,124 @@ namespace Libraryms.Controllers
                 return NotFound();
             }
 
-            var klientet = await _context.Rezervimi
+            var rezervimi = await _context.Rezervimi
                 .FirstOrDefaultAsync(m => m.id == id);
 
-            if (klientet == null)
+            if (rezervimi == null)
             {
                 return NotFound();
             }
 
-            return View(rezervimet);
+            return View(rezervimi);
+        }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var r = await _context.Rezervimi.FindAsync(id);
+            if (r == null)
+            {
+                return NotFound();
+            }
+            return View(r);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Klienti_id,Libra_id,Aktiv,created_at,deleted_at")] Rezervimi r)
+        {
+            if (id != r.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(r);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RezervimiExists(r.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+
+                        throw;
+
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(r);
+        }
+        [HttpGet]
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Create(
+            [Bind("Klienti_id,Libra_id,Aktiv")] Rezervimi r)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Add(r);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (DbUpdateException /* ex */)
+            {
+                //Log the error (uncomment ex variable name and write a log.
+                ModelState.AddModelError("", "Unable to save changes. " +
+                    "Try again, and if the problem persists " +
+                    "see your system administrator.");
+            }
+            return View(r);
+
+        }
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var r = await _context.Rezervimi
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (r == null)
+            {
+                return NotFound();
+            }
+
+            return View(r);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var r = await _context.Rezervimi.FindAsync(id);
+            _context.Klienti.Remove(r);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        private bool RezervimiExists(int id)
+        {
+            return _context.Rezervimi.Any(e => e.id == id);
         }
     }
+}
