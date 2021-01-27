31b3bbe0d99e69545b35df7558f4ea0b995b1315
leutrimahmeti <trimmahmeti@outlook.com>
2021-01-27 17:39:43 +0100

    roles done

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,4 +1,5 @@
 ﻿using Libraryms.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace Libraryms.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
         private readonly RoleManager<IdentityRole> roleManager;
