bca4fefcf02e86abdd744926d72f875e96d601e8
Asdren Xhafa <asdrenxhafa@gmail.com>
2021-01-27 22:19:57 +0100

    small fixes on js files

--- Controllers/StatistikaController.cs
+++ Controllers/StatistikaController.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Libraryms.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Libraryms.Controllers
 {
     public class StatistikaController : Controller
     {
+        private readonly LibrarymsContext _context;
+
+        public StatistikaController(LibrarymsContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
