69b184515e2d0c7cf1fff2ab9c70166cd06b19b5
Asdren Xhafa <asdrenxhafa@gmail.com>
2021-01-28 20:55:45 +0100

    fix statistika area function

--- Controllers/StatistikaController.cs
+++ Controllers/StatistikaController.cs
@@ -23,44 +23,43 @@ namespace Libraryms.Controllers
 
         [HttpGet]
 
-        public ActionResult<IEnumerable<IEnumerable<string>>> Area()
+        public ActionResult<IEnumerable<IEnumerable<int>>> Area()
         {
             var jan = 
-                _context.Pagesa.Where(p => p.created_at.Month == 1)
-                .Select(p => p.shuma).ToList();
+                _context.Pagesa.Where(p => p.DataEPageses.Month == 1).Select(p => p.Shuma).ToList();
 
             var feb =
-                _context.Pagesa.Where(p => p.created_at.Month == 2).Select(p => p.shuma).ToList();
+                _context.Pagesa.Where(p => p.DataEPageses.Month == 2).Select(p => p.Shuma).ToList();
 
             var mar =
-                _context.Pagesa.Where(p => p.created_at.Month == 3).Select(p => p.shuma).ToList();
+                _context.Pagesa.Where(p => p.DataEPageses.Month == 3).Select(p => p.Shuma).ToList();
 
             var apr =
-                _context.Pagesa.Where(p => p.created_at.Month == 4).Select(p => p.shuma).ToList();
+                _context.Pagesa.Where(p => p.DataEPageses.Month == 4).Select(p => p.Shuma).ToList();
 
             var may =
-                _context.Pagesa.Where(p => p.created_at.Month == 5).Select(p => p.shuma).ToList();
+                _context.Pagesa.Where(p => p.DataEPageses.Month == 5).Select(p => p.Shuma).ToList();
 
             var jun =
-                _context.Pagesa.Where(p => p.created_at.Month == 6).Select(p => p.shuma).ToList();
+                _context.Pagesa.Where(p => p.DataEPageses.Month == 6).Select(p => p.Shuma).ToList();
 
             var jul =
-                _context.Pagesa.Where(p => p.created_at.Month == 7).Select(p => p.shuma).ToList();
+                _context.Pagesa.Where(p => p.DataEPageses.Month == 7).Select(p => p.Shuma).ToList();
 
             var aug =
-                _context.Pagesa.Where(p => p.created_at.Month == 8).Select(p => p.shuma).ToList();
+                _context.Pagesa.Where(p => p.DataEPageses.Month == 8).Select(p => p.Shuma).ToList();
 
             var sep =
-                _context.Pagesa.Where(p => p.created_at.Month == 9).Select(p => p.shuma).ToList();
+                _context.Pagesa.Where(p => p.DataEPageses.Month == 9).Select(p => p.Shuma).ToList();
 
             var oct =
-                _context.Pagesa.Where(p => p.created_at.Month == 10).Select(p => p.shuma).ToList();
+                _context.Pagesa.Where(p => p.DataEPageses.Month == 10).Select(p => p.Shuma).ToList();
 
             var nov =
-                _context.Pagesa.Where(p => p.created_at.Month == 11).Select(p => p.shuma).ToList();
+                _context.Pagesa.Where(p => p.DataEPageses.Month == 11).Select(p => p.Shuma).ToList();
 
             var dec =
-                _context.Pagesa.Where(p => p.created_at.Month == 12).Select(p => p.shuma).ToList();
+                _context.Pagesa.Where(p => p.DataEPageses.Month == 12).Select(p => p.Shuma).ToList();
 
 
             return new[]
