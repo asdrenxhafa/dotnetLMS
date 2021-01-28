0d3ee4e67e62ea91b8b4ca8b95bfc414681f8ca9
Asdren Xhafa <asdrenxhafa@gmail.com>
2021-01-28 20:36:01 +0100

    api function for returning Area chart of pagesa

--- Controllers/StatistikaController.cs
+++ Controllers/StatistikaController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Libraryms.Data;
+using Libraryms.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Libraryms.Controllers
@@ -19,6 +20,66 @@ namespace Libraryms.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+
+        public ActionResult<IEnumerable<IEnumerable<string>>> Area()
+        {
+            var jan = 
+                _context.Pagesa.Where(p => p.created_at.Month == 1)
+                .Select(p => p.shuma).ToList();
+
+            var feb =
+                _context.Pagesa.Where(p => p.created_at.Month == 2).Select(p => p.shuma).ToList();
+
+            var mar =
+                _context.Pagesa.Where(p => p.created_at.Month == 3).Select(p => p.shuma).ToList();
+
+            var apr =
+                _context.Pagesa.Where(p => p.created_at.Month == 4).Select(p => p.shuma).ToList();
+
+            var may =
+                _context.Pagesa.Where(p => p.created_at.Month == 5).Select(p => p.shuma).ToList();
+
+            var jun =
+                _context.Pagesa.Where(p => p.created_at.Month == 6).Select(p => p.shuma).ToList();
+
+            var jul =
+                _context.Pagesa.Where(p => p.created_at.Month == 7).Select(p => p.shuma).ToList();
+
+            var aug =
+                _context.Pagesa.Where(p => p.created_at.Month == 8).Select(p => p.shuma).ToList();
+
+            var sep =
+                _context.Pagesa.Where(p => p.created_at.Month == 9).Select(p => p.shuma).ToList();
+
+            var oct =
+                _context.Pagesa.Where(p => p.created_at.Month == 10).Select(p => p.shuma).ToList();
+
+            var nov =
+                _context.Pagesa.Where(p => p.created_at.Month == 11).Select(p => p.shuma).ToList();
+
+            var dec =
+                _context.Pagesa.Where(p => p.created_at.Month == 12).Select(p => p.shuma).ToList();
+
+
+            return new[]
+                {
+                    jan,
+                    feb,
+                    mar,
+                    apr,
+                    may,
+                    jun,
+                    jul,
+                    aug,
+                    sep,
+                    oct,
+                    nov,
+                    dec
+                };
+        }
+
 
 
     }
